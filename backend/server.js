7bffb504782acca222e192379b69b9944f3465cd
Samuel <samuelteweldebrhan@gmail.com>
2024-03-19 12:14:45 +0300

    install mongoose and configure notesdb

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-    res.send("This is the Home Page");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Samitti:Sam1984@cluster0.u6jymdm.mongodb.net/notesdb').then(function(){
+    app.get('/', function(req, res){
+        res.send("This is the Home Page");
+    });
+    
+    app.get('/notes', function(req, res){
+        res.send('This is notes page');
+    });
 });
 
-app.get('/notes', function(req, res){
-    res.send('This is notes page');
-});
 
 app.listen('3000', ()=>{
     console.log("Server running at PORT: 3000");
